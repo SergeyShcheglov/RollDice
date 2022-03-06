46552f174fcf9dcfa1ca95063051ea656cffbf80
Sergey Shcheglov <97shcheglov@gmail.com>
2022-03-06 20:26:52 +0600

    first commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Sergey Shcheglov on 06.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
