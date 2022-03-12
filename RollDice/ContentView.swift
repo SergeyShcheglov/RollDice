9bb74f005fe688e69904dd216917e305533fa2cc
Sergey Shcheglov <97shcheglov@gmail.com>
2022-03-12 19:19:15 +0600

    Fix bug with removing dices

--- RollDice/ContentView.swift
+++ RollDice/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var dices = 1
-    @State private var diceValue = 6
+    @State private var diceAmount = Array(0...6)
 
     @State private var defaultSides = 6
     private let sides = [4, 6, 8, 10, 12, 20, 100]
     
     @State private var previousResult = 0
     @State private var totalResult = 0
+    
+    @State private var diceDict: [String: Int] = ["1Dice": 6]
     
     var body: some View {
         NavigationView {
@@ -30,13 +32,13 @@ struct ContentView: View {
                 
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack {
-                    ForEach(0..<dices, id:\.self) { dice in
+                        ForEach(diceDict.values.sorted(), id:\.self) { dice in
                             ZStack {
                                 RoundedRectangle(cornerRadius: 25)
                                     .foregroundColor(Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1)))
                                     
                                 
-                                Text(diceValue.description)
+                                Text(String(dice))
                                     .font(.largeTitle)
                                     .foregroundColor(.white)
                             }
@@ -44,18 +46,32 @@ struct ContentView: View {
                         }
                     }
                 }
-               
-                
-                Button("Roll dice") {
-                    rollDice()
+                Button {
+                    withAnimation {
+                        rollDice()
+                    }
+                } label: {
+                    Text("Roll dice")
+                        .font(.title2)
+                        .padding().frame(height: 50)
                 }
                 Spacer()
                 
                 Form {
+                    
+                   
                     Stepper("Dices: \(dices)", value: $dices, in: 1...6)
+                    {
+                        dice in
+                        changeDicesAmount(diceKey: "\(dices)Dice", diceValue: Int.random(in: 1...6))
+                    }
                     
                     Section("Side amount") {
-                        
+//                        Picker("Dices", selection: $dices ) {
+//                            ForEach(diceAmount, id:\.self) {
+//                                Text($0.description)
+//                            }
+//                        }
                         Picker("Side", selection: $defaultSides) {
                             ForEach(sides, id: \.self) {
                                 Text($0.description)
@@ -69,7 +85,39 @@ struct ContentView: View {
     }
     
     func rollDice() {
-        diceValue = Int.random(in: 1...defaultSides)
+//        diceValue = Int.random(in: 1...defaultSides)
+        for (key, _) in diceDict {
+            diceDict[key] = Int.random(in: 1...defaultSides)
+        }
+        sumTotal()
+    }
+    
+    func createNewDice(diceKey: String, diceValue: Int) {
+        diceDict[diceKey] = diceValue
+        print(diceDict)
+    }
+    
+    func changeDicesAmount(diceKey: String, diceValue: Int) {
+        if dices > diceDict.count {
+            diceDict[diceKey] = diceValue
+        } else if dices < diceDict.values.count {
+            let diceRandom = diceDict.randomElement()?.key
+            diceDict.removeValue(forKey: diceRandom!)
+        }
+        print(dices)
+        print(diceDict.description)
+    }
+    
+    func sumTotal() {
+        var result: Int {
+            var total = 0
+        for (key, _) in diceDict {
+            total += diceDict[key]!
+        }
+            return total
+    }
+        previousResult = totalResult
+        totalResult = result
     }
 }
 
